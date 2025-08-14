8b672a1f4c0b1635beb802fd443c0099947412eb
simoes-source <joaopedro.simoes80@gmail.com>
2025-08-14 23:28:27 +0000

    Criação do primeiro programa em C

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    //saída de dados em C
+    printf("Meu primeiro programa em C!\n");
+    printf("Agora estou preprarado para os desafios em C\n");
+  
+    return 0;
+}
